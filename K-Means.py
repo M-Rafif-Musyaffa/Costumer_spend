1423011b60d40ee75e1a95640703b51fb5554c3b
M Rafif Musyaffa <88952324+M-Rafif-Musyaffa@users.noreply.github.com>
2023-12-30 19:42:53 +0700

    Update K-Means.py

--- K-Means.py
+++ K-Means.py
@@ -36,8 +36,8 @@ plt.title("Elbow Curve")
 st.set_option("deprecation.showPyplotGlobalUse", False)
 elbo_plot = st.pyplot()
 
-st.sidebar.subheader("Nilai jumlah K")
-clust = st.sidebar.slider("Pilih jumlah cluster :", 2, 10, 3, 1)
+st.header("Nilai jumlah K")
+clust = st.sidebar("Pilih jumlah cluster :", 2, 10, 3, 1)
 
 
 def k_means(best_k):
